4e5070448790058791320a1ad4ada3f76f32a020
yukunfeng <yukunfg@gmail.com>
2021-08-29 16:53:45 +0900

    update

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -x
 
 data_path="./data/zh"
 
-# python -u main.py --dropout 0.5  --input_freq 1 --max_gram_n 3 --note "char_lm" \
-    # --data $data_path --epoch 40 --emsize 650 --nhid 650
+python -u main.py --dropout 0.5  --input_freq 1 --max_gram_n 3 --note "char_lm" \
+    --data $data_path --epoch 40 --emsize 650 --nhid 650
 
 python -u main.py --dropout 0.5 --input_freq 1 --max_gram_n 3 --note "char_lm_warmup" \
     --data $data_path --epoch 40 --emsize 650 --nhid 650 --use_warmup \
